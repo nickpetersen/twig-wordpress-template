a7597fcd2e7081628211d1904a13103bc5e6fa47
RJ Nay <rjnay1984@gmail.com>
2019-01-11 16:36:32 -0600

    init commit

---
+++ gulpfile.babel.js
@@ -0,0 +1,65 @@
+const { watch, dest, src, series } = require('gulp');
+const browserSync = require('browser-sync').create();
+const scss = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const imagemin = require('gulp-imagemin');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const babel = require('gulp-babel');
+const sourcemaps = require('gulp-sourcemaps');
+const reload = browserSync.reload;
+
+function css() {
+  return src('assets/sass/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(
+      scss({
+        outputStyle: 'compressed',
+        includePaths: './node_modules/'
+      }).on('error', scss.logError)
+    )
+    .pipe(
+      autoprefixer({
+        browsers: 'last 2 versions'
+      })
+    )
+    .pipe(sourcemaps.write())
+    .pipe(dest('css/'))
+    .pipe(browserSync.stream());
+}
+
+function images() {
+  return src('assets/images/*')
+    .pipe(imagemin())
+    .pipe(dest('images/'));
+}
+
+function vendor() {
+  return src([
+    'node_modules/jquery/dist/jquery.js',
+    'node_modules/popper.js/dist/umd/popper.js',
+    'node_modules/bootstrap/dist/js/bootstrap.js'
+  ])
+    .pipe(concat('vendors.js'))
+    .pipe(uglify())
+    .pipe(dest('js/'));
+}
+
+function js() {
+  return src('assets/js/*.js')
+    .pipe(babel())
+    .pipe(uglify())
+    .pipe(dest('js/'));
+}
+
+exports.serve = series(css, images, vendor, js, () => {
+  browserSync.init({
+    proxy: 'http://twigdemo.test',
+    notify: false
+  });
+  watch('assets/sass/**/**/*.scss', css);
+  watch('templates/**/**/**/*.twig').on('change', reload);
+  watch('assets/js/*.js', js).on('change', reload);
+});
+
+exports.default = series(css, images, vendor, js);
