0fe5cd725e452e554bf2465365d8d7bee0a394dd
Nick Petersen <nick@nickpetersen.co>
2019-08-07 09:32:58 -0500

    mobile nav

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,72 @@
-const message = 'Hello World';
+let navLines = document.getElementById("navLines");
+let page = document.getElementById("wrapper");
+let clicked = false;
 
-console.log(message);
+if (nav.classList.contains("show")) {
+    page.onclick = function() { toggleMenu() };  
+}
+
+page.onclick = function() {
+    if (nav.classList.contains("show")) {
+        toggleMenu()
+    };
+};
+
+
+navLines.onclick = function() { toggleMenu() };
+
+function toggleMenu () {
+  let nav = document.getElementById('nav');
+
+  nav.classList.toggle("show");
+
+  if (nav.classList.contains("show")) { crossNavLines(); } 
+  else { navLinesParrallel() }
+}
+
+function isHover(hoveredElement) {
+  return (hoveredElement.parentElement.querySelector(':hover') === hoveredElement);
+}
+
+document.addEventListener('click', function (event) {
+
+  if (!event.target.matches('.navLink')) return;
+    nav.classList.toggle("show");
+    navLinesParrallel()
+}, false);
+
+
+function crossNavLines () {
+  clicked = true;
+
+  topLine.style.marginTop = "0px";
+  topLine.style.transform = "rotate(-60deg)";
+  topLine.style.transition = "all .2s ease";
+
+  middleLine.style.marginTop = "-14px";
+  middleLine.style.marginLeft = "28px";
+  middleLine.style.transform = "rotate(65deg)";
+  middleLine.style.transition = "all .2s ease";
+
+  bottomLine.style.marginTop = "21px";
+  bottomLine.style.marginLeft = "13px";
+  bottomLine.style.width = "20%";
+  bottomLine.style.transform = "rotate(0deg)";
+  bottomLine.style.transition = "all .2s ease";
+}
+
+function navLinesParrallel () {
+clicked = false;
+topLine.style.marginTop = "0px";
+topLine.style.transform = "rotate(0deg)";
+
+middleLine.style.marginTop = "-0";
+middleLine.style.marginLeft = "0";
+middleLine.style.transform = "rotate(0deg)";
+
+bottomLine.style.marginTop = "0";
+bottomLine.style.marginLeft = "0";
+bottomLine.style.width = "15%";
+bottomLine.style.transform = "rotate(0deg)";
+}
+
--- page.php
+++ page.php
@@ -24,4 +24,5 @@
 $context = Timber::get_context();
 $post = new TimberPost();
 $context['post'] = $post;
+$context['sidebar'] = Timber::get_sidebar('sidebar.php');
 Timber::render( array( 'page-' . $post->post_name . '.twig', 'page.twig' ), $context );
